ea592684911af0b50e63f3673d605bc1cf491ebe
Yoon-Jemin <yjm1800@naver.com>
2024-08-08 14:41:00 +0900

    [feat] N으로 표현

---
+++ src/programmers/동적계획법/N으로표현.java
@@ -0,0 +1,52 @@
+package programmers.동적계획법;
+
+import java.util.*;
+
+public class N으로표현 {
+
+    public static void main(String[] args) {
+        int N = 5;
+        int number = 12;
+        System.out.println(solution(N,number));
+    }
+
+    public static int solution(int N, int number) {
+
+        int answer = -1;
+
+        Set<Integer>[] setArr = new Set[9];
+
+        // 초기화
+        int t = N;
+        for(int i = 1; i < 9; i++){
+            setArr[i] = new HashSet<>();
+            setArr[i].add(t);
+            t = t * 10 + N;
+        }
+
+        for(int i = 1; i < 9; i ++){
+            for(int j = 1; j < i; j++){
+                for(Integer a : setArr[j]){     // j개 만큼의 N을 쓴 숫자 모음
+                    for(Integer b : setArr[i - j]){     // (i - j)개 만큼의 N을 쓴 숫자 모음
+                        setArr[i].add(a + b);
+                        setArr[i].add(a - b);
+                        setArr[i].add(b - a);
+                        setArr[i].add(a * b);
+                        if(b != 0) setArr[i].add(a / b);
+                        if(a != 0) setArr[i].add(b / a);
+                    }
+                }
+            }
+        }
+
+        for(int i = 1; i < 9; i++){
+            if(setArr[i].contains(number)){
+                answer = i;
+                break;
+            }
+        }
+
+        return answer;
+
+    }
+}
