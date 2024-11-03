e9c081cb463c4f8f0663303958acf99c9fa2e322
Yoon-Jemin <yjm1800@naver.com>
2024-11-03 19:02:38 +0900

    longestPalindrome

---
+++ src/leetcode/DP/longestPalindrome.java
@@ -0,0 +1,41 @@
+package leetcode.DP;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+public class longestPalindrome {
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("s"));
+    }
+
+    public static String longestPalindrome(String s) {
+        int size = s.length();
+
+        if(size == 1) return s;
+
+        int maxlen = 1;
+        String maxStr = s.substring(0, 1);
+
+        for (int i = 0; i < size; i++) {
+            for (int j = maxlen + 1; j <= size; j++) {
+                if(j-i > maxlen && isPalindrome(s.substring(i, j))) {
+                    maxlen = j-i;
+                    maxStr = s.substring(i, j);
+                }
+            }
+        }
+
+        return maxStr;
+    }
+
+    private static boolean isPalindrome(String word) {
+        int size = word.length();
+        for(int i = 0; i < size / 2; i++) {
+            if(word.charAt(i) != word.charAt(size - i - 1)) return false;
+        }
+        return true;
+    }
+}
