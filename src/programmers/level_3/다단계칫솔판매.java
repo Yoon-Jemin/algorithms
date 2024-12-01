1fe08de5b14179987c344b29bd77db1bae6b9131
Yoon-Jemin <yjm1800@naver.com>
2024-12-02 02:07:34 +0900

    다단계 칫솔 판매

--- src/programmers/level_3/다단계칫솔판매.java
+++ src/programmers/level_3/다단계칫솔판매.java
@@ -1,17 +1,55 @@
 package programmers.level_3;
 
+import java.util.*;
+
 public class 다단계칫솔판매 {
 
     public static void main(String[] args) {
         String[] enroll = {"john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"};
         String[] referral = {"-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"};
-        String[] seller = {"young", "john", "tod", "emily", "mary"};
-        int[] amount = {12, 4, 2, 5, 10};
-        int[] result = {360, 958, 108, 0, 450, 18, 180, 1080};
+        String[] seller = {"sam", "emily", "jaimie", "edward"};
+        int[] amount = {2, 3, 5, 4};
+        int[] answer = solution(enroll, referral, seller, amount);
+        System.out.println(Arrays.toString(answer));
     }
 
-    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        int[] answer = {};
-        return answer;
+    static int[] profit;
+    static List<String>[] graph;
+    static Map<String, Integer> map;
+    public static int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        profit = new int[enroll.length];
+        graph = new ArrayList[enroll.length];
+        map = new HashMap<>();     // 이름 : 인덱스
+
+        for (int i = 0; i < enroll.length; i++) {
+            graph[i] = new ArrayList<>();
+            map.put(enroll[i], i);
+        }
+
+        for (int i = 0; i < referral.length; i++) {
+            if (referral[i] == "-") continue;
+            graph[map.get(enroll[i])].add(referral[i]);
+        }
+
+        for (int i = 0; i < seller.length; i++) {
+            DFS(seller[i], amount[i] * 100);
+        }
+
+        return profit;
+    }
+
+    private static void DFS(String seller, int price) {
+        if (!map.containsKey(seller)) return;
+
+        if (price >= 10){
+            int give = (price / 10) ;
+            int mine = price - give;
+            profit[map.get(seller)] += mine;
+            if(!graph[map.get(seller)].isEmpty()){
+                DFS(graph[map.get(seller)].get(0), give);
+            }
+        } else {
+            profit[map.get(seller)] += price;
+        }
     }
 }
