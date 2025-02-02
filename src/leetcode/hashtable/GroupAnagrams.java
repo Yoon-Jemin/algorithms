9866218c9d3fa438d4410127db4e163363bb5128
Yoon-Jemin <yjm1800@naver.com>
2025-02-02 23:42:56 +0900

    Group Anagrams

---
+++ src/leetcode/hashtable/GroupAnagrams.java
@@ -0,0 +1,26 @@
+package leetcode.hashtable;
+
+import java.util.*;
+
+public class GroupAnagrams {
+
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+
+            map.computeIfAbsent(sortedStr, k -> new ArrayList<>()).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+}
