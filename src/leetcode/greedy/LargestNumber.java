fee97c3dce51a3132140300a64d130dea6e3b0e1
Yoon-Jemin <yjm1800@naver.com>
2025-03-05 22:09:17 +0900

    Remove Duplicate Letters

--- src/leetcode/greedy/LargestNumber.java
+++ src/leetcode/greedy/LargestNumber.java
@@ -1,4 +1,27 @@
 package leetcode.greedy;
 
+import java.util.Arrays;
+
 public class LargestNumber {
+
+    public static void main(String[] args) {
+        int[] nums = {10,2,9,39,17};
+        System.out.println(largestNumber(nums));    // "9534330"
+    }
+
+    public static String largestNumber(int[] nums) {
+        String[] strs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            strs[i] = String.valueOf(nums[i]);
+        }
+
+        Arrays.sort(strs, (a, b) -> (b + a).compareTo(a + b));
+        StringBuilder sb = new StringBuilder();
+        if (strs[0].equals("0")) return "0";
+        for (int i = 0; i < strs.length; i++) {
+            sb.append(strs[i]);
+        }
+
+        return sb.toString();
+    }
 }
