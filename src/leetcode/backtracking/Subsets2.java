63d72f26e50b13b2b0c6e16c5989228678eedcce
Yoon-Jemin <yjm1800@naver.com>
2025-04-16 19:51:39 +0900

    [leetcode] subset2

--- src/leetcode/backtracking/Subsets2.java
+++ src/leetcode/backtracking/Subsets2.java
@@ -10,16 +10,17 @@ public class Subsets2 {
     }
 
     public static List<List<Integer>> subsetsWithDup(int[] nums) {
-        Set<List<Integer>> res = new HashSet<>();
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
         backTrack(0, new ArrayList<>(), nums, res);
         return new ArrayList<>(res);
     }
 
-    private static void backTrack(int index, List<Integer> list, int[] nums, Set<List<Integer>> res) {
+    private static void backTrack(int index, List<Integer> list, int[] nums, List<List<Integer>> res) {
         res.add(new ArrayList<>(list));
 
         for (int i = index; i < nums.length; i++) {
+            if (i > index && nums[i] == nums[i - 1]) continue;
             list.add(nums[i]);
             backTrack(i + 1, list, nums, res);
             list.remove(list.size() - 1);
