7e1971ff4cefac71a4cf52a88c110974913da3b7
Yoon-Jemin <yjm1800@naver.com>
2025-10-15 21:02:20 +0900

    [백준] 가장 긴 감소하는 부분수열

---
+++ src/baekjoon/동적계획법/가장긴감소하는부분수열.java
@@ -0,0 +1,38 @@
+package baekjoon.동적계획법;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 가장긴감소하는부분수열 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int answer = 1;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[i]) {  // 감소 가능
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    answer = Math.max(answer, dp[i]);
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
